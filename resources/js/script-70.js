7653907e8a59fd8ec3c6fbbd3b649aa6af80d852
webcodem-srbig <garciarojocsa@gmail.com>
2025-02-24 16:08:30 -0600

    first commit

---
+++ resources/js/script-70.js
@@ -0,0 +1,5 @@
+var idioma = navigator.language || navigator.userLanguage; 
+
+        if (idioma.startsWith('en')) {
+            window.location.href = 'https://webcodem.github.io/marketplace-en-us/actions-&-stuff'; 
+        }
