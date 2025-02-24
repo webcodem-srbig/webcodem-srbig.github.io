584f4ccc5d5e5abb49e71a8bb8e92566d772dce3
webcodem-srbig <garciarojocsa@gmail.com>
2025-02-24 16:07:52 -0600

    first commit

---
+++ resources/js/script-66.js
@@ -0,0 +1,5 @@
+       var idioma = navigator.language || navigator.userLanguage; 
+
+        if (idioma.startsWith('en')) {
+            window.location.href = 'https://webcodem.github.io/en-us'; 
+        }
