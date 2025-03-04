d18ca5e15adc7e737e45fa6a789b8fdb11db78e2
webcodem-srbig <garciarojocsa@gmail.com>
2025-03-04 14:47:53 -0600

    fixes en-us

--- resources/js/script-66.js
+++ resources/js/script-66.js
@@ -1,5 +1,8 @@
-       var idioma = navigator.language || navigator.userLanguage; 
-
-        if (idioma.startsWith('en')) {
-            window.location.href = 'https://webcodem-srbig.github.io/en-us'; 
-        }
+// Desactivar redirección cuando ya estamos en la versión en inglés
+var currentPath = window.location.pathname;
+if (!currentPath.includes("/en-us/")) {
+    var idioma = navigator.language || navigator.userLanguage; 
+    if (idioma.startsWith('en')) {
+        window.location.href = 'https://webcodem-srbig.github.io/en-us'; 
+    }
+}
