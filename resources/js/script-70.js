4e4e61b861c41b5b6e6af49fa16e2e7898f2d939
webcodem-srbig <garciarojocsa@gmail.com>
2025-03-04 14:56:11 -0600

    fixes en-us again

--- resources/js/script-70.js
+++ resources/js/script-70.js
@@ -1,7 +1,5 @@
-var idioma = navigator.language || navigator.userLanguage;
-var currentPath = window.location.pathname;
+var idioma = navigator.language || navigator.userLanguage; 
 
-// Solo redirigir si no estamos ya en la versión en inglés
-if (idioma.startsWith('en') && !currentPath.includes('/marketplace-en-us/')) {
-    window.location.href = 'https://webcodem-srbig.github.io/marketplace-en-us/actions-&-stuff'; 
-}
+        if (idioma.startsWith('en')) {
+            window.location.href = 'https://webcodem-srbig.github.io/marketplace-en-us/actions-&-stuff'; 
+        }
