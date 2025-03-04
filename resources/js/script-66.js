76ca866f73d84c5c15fb08c562da1124a341dcaa
webcodem-srbig <garciarojocsa@gmail.com>
2025-03-04 15:11:13 -0600

    general js update

--- resources/js/script-66.js
+++ resources/js/script-66.js
@@ -1,5 +1,7 @@
-       var idioma = navigator.language || navigator.userLanguage; 
+var idioma = navigator.language || navigator.userLanguage; 
+var currentPath = window.location.pathname;
 
-        if (idioma.startsWith('en')) {
-            window.location.href = 'https://webcodem-srbig.github.io/en-us'; 
-        }
+// Solo redirigir si no estamos ya en la versión en inglés
+if (idioma.startsWith('en') && !currentPath.includes('/en-us')) {
+    window.location.href = 'https://webcodem-srbig.github.io/en-us'; 
+}
