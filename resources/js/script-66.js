5d4143596af1d7f2c5776071cef3ad26c4ee48ed
webcodem-srbig <garciarojocsa@gmail.com>
2025-03-04 14:37:28 -0600

    en-us language support

--- resources/js/script-66.js
+++ resources/js/script-66.js
@@ -1,5 +1,5 @@
        var idioma = navigator.language || navigator.userLanguage; 
 
         if (idioma.startsWith('en')) {
-            window.location.href = 'https://webcodem.github.io/en-us'; 
+            window.location.href = 'https://webcodem-srbig.github.io/en-us'; 
         }
--- resources/js/script-71.js
+++ resources/js/script-71.js
@@ -1,5 +1,5 @@
 var idioma = navigator.language || navigator.userLanguage; 
 
         if (idioma.startsWith('en')) {
-            window.location.href = 'https://webcodem-srbig.github.io/marketplace-en-us/actions-&-stuff'; 
+            window.location.href = 'https://webcodem-srbig.github.io/marketplace-en-us/prime'; 
         }
